999f356c8097f971a81bc3212cc370c177ac6def
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2025-01-15 20:55:46 -0300

    remove files

--- src/main/java/com/commit/sync/controller/WebhookController.java
+++ src/main/java/com/commit/sync/controller/WebhookController.java
@@ -1,15 +1,11 @@
 package com.commit.sync.controller;
 
-import com.commit.sync.model.Commit;
 import com.commit.sync.model.Repository;
-import com.commit.sync.service.EventoService;
-import java.util.List;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,10 +20,11 @@ public class WebhookController {
   private static final Logger logger = LoggerFactory.getLogger(WebhookController.class);
 
   @PostMapping("/webhook")
-  public ResponseEntity<Repository> handleWebhook(@RequestBody Repository repository) {
-    System.out.println("Repositorio Recebido: " + repository);
+  public ResponseEntity<Map<String, Object>> handleWebhook(@RequestBody Map<String, Object> payload) {
+    System.out.println("Repositorio Recebido: " + payload);
+    logger.info("Event: {}",payload);
 
-    return ResponseEntity.status(HttpStatus.OK).body(repository);
+    return ResponseEntity.status(HttpStatus.OK).body(payload);
   }
 
 }
